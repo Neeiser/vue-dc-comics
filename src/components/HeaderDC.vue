de1dedacd28659034884454348088cea1e1337dc
Neeiser <stortini.study@gmail.com>
2022-03-22 16:16:41 +0100

    SCSS initial structure

--- src/components/HeaderDC.vue
+++ src/components/HeaderDC.vue
@@ -59,6 +59,12 @@ export default {
 }
 </script>
 
-<style>
-
+<style scoped lang="scss">
+li{
+    display: inline-block;
+    a{
+        display: inline-block;
+        text-decoration: none;
+    }
+}
 </style>
