742e7b474f1957b75e09878c5fccf71cb2892d35
Neeiser <stortini.study@gmail.com>
2022-03-22 19:02:03 +0100

    footer data() li structure

--- src/components/FooterDC.vue
+++ src/components/FooterDC.vue
@@ -1,8 +1,14 @@
 <template>
   <footer>
       <div class="container">
-          <div></div>
-          <div></div>
+          <div>
+              <ul>
+                  <li></li>
+              </ul>
+          </div>
+          <div>
+              <img src="../assets/img/dc-logo-bg.png" alt="">
+          </div>
       </div>
   </footer>
 </template>
@@ -12,6 +18,51 @@ export default {
     name: 'FooterDC',
     data () {
         return {
+            sections:[
+                {
+                    name: 'DC COMICS',
+                    subsection:[
+                        {text:'Characters'},
+                        {text:'Comics'},
+                        {text:'Movies'},
+                        {text:'TV'},
+                        {text:'Games'},
+                        {text:'Videos'},
+                        {text:'News'}
+                    ]
+                },
+                {
+                    name: 'DC SHOP',
+                    subsection:[
+                        {text:'Shop DC'},
+                        {text:'Shop DC Collectibles'}
+                    ]
+                },
+                {
+                    name: 'DC',
+                    subsection:[
+                        {text:'Terms Of Use'},
+                        {text:'Privacy Policy (New)'},
+                        {text:'Advertising'},
+                        {text:'Jobs'},
+                        {text:'Subscriptions'},
+                        {text:'CPSC Certificates'},
+                        {text:'Ratings'},
+                        {text:'Shop Help'},
+                        {text:'Contact Us'},
+                    ]
+                },
+                {
+                    name: 'SITE',
+                    subsection:[
+                        {text:'DC'},
+                        {text:'DC UNIVERSE INFINITE'},
+                        {text:'DC Kids'},
+                        {text:'DC Shop'},
+                        {text:'MAD Magazine'}
+                    ]
+                },
+            ]
         }
     }
 }
@@ -25,4 +76,9 @@ footer{
     background-repeat: no-repeat;
     background-size: cover;
 }
+
+img{
+    height: 200px;
+}
+
 </style>
